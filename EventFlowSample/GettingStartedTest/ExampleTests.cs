d8f4cbf4ad9e0927f2299a92a8ac1479bb76e6c3
SergerGood <serzher@inbox.ru>
2019-10-04 14:25:39 +0300

    distinct

--- EventFlowSample/GettingStartedTest/ExampleTests.cs
+++ EventFlowSample/GettingStartedTest/ExampleTests.cs
@@ -1,8 +1,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using EventFlow;
-using EventFlow.Aggregates.ExecutionResults;
-using EventFlow.Configuration;
+using EventFlow.Exceptions;
 using EventFlow.Extensions;
 using EventFlow.Queries;
 using FluentAssertions;
@@ -14,37 +13,65 @@ namespace GettingStartedTest
     public class ExampleTests
     {
         [Test]
-        public async Task GettingStartedExample()
+        public async Task ShouldHandleCommand()
         {
             using (var tokenSource = new CancellationTokenSource())
             {
-                using (IRootResolver resolver = EventFlowOptions.New
+                using (var resolver = EventFlowOptions.New
                     .AddEvents(typeof(Event))
                     .AddCommands(typeof(SetMagicNumberCommand))
                     .AddCommandHandlers(typeof(SetMagicNumberCommandHandler))
                     .UseInMemoryReadStoreFor<ReadModel>()
                     .CreateResolver())
                 {
-                    AggregateId exampleId = AggregateId.NewComb();
+                    var exampleId = AggregateId.NewComb();
                     const int magicNumber = 42;
 
                     var commandBus = resolver.Resolve<ICommandBus>();
 
                     var command = new SetMagicNumberCommand(exampleId, magicNumber);
-                    IExecutionResult executionResult1 = await commandBus.PublishAsync(command, tokenSource.Token);
+                    var executionResult1 = await commandBus.PublishAsync(command, tokenSource.Token);
 
                     var command2 = new SetMagicNumberCommand(exampleId, magicNumber);
-                    IExecutionResult executionResult2 = await commandBus.PublishAsync(command2, tokenSource.Token);
+                    var executionResult2 = await commandBus.PublishAsync(command2, tokenSource.Token);
 
                     executionResult1.IsSuccess.Should().BeTrue();
                     executionResult2.IsSuccess.Should().BeFalse();
 
                     var queryProcessor = resolver.Resolve<IQueryProcessor>();
-                    
+
                     var query = new ReadModelByIdQuery<ReadModel>(exampleId);
-                    ReadModel readModel = await queryProcessor.ProcessAsync(query, tokenSource.Token);
+                    var readModel = await queryProcessor.ProcessAsync(query, tokenSource.Token);
 
                     readModel.MagicNumber.Should().Be(magicNumber);
+                }
+            }
+        }
+
+        [Test]
+        public async Task ShouldThrowDuplicateOperationException()
+        {
+            using (var tokenSource = new CancellationTokenSource())
+            {
+                using (var resolver = EventFlowOptions.New
+                    .AddEvents(typeof(Event))
+                    .AddCommands(typeof(SetMagicNumberDistinctCommand))
+                    .AddCommandHandlers(typeof(SetMagicNumberDistinctCommandHandler))
+                    .UseInMemoryReadStoreFor<ReadModel>()
+                    .CreateResolver())
+                {
+                    var exampleId = AggregateId.NewComb();
+                    const int magicNumber = 42;
+
+                    var commandBus = resolver.Resolve<ICommandBus>();
+
+                    var command = new SetMagicNumberDistinctCommand(exampleId, magicNumber);
+                    await commandBus.PublishAsync(command, tokenSource.Token);
+
+                    var command2 = new SetMagicNumberDistinctCommand(exampleId, magicNumber);
+
+                    Assert.ThrowsAsync<DuplicateOperationException>(async () =>
+                        await commandBus.PublishAsync(command2, tokenSource.Token));
                 }
             }
         }
---
+++ EventFlowSample/GettingStartedTest/Model/SetMagicNumberDistinctCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using EventFlow.Aggregates.ExecutionResults;
+using EventFlow.Commands;
+
+namespace GettingStartedTest.Model
+{
+    public class SetMagicNumberDistinctCommandHandler
+        : CommandHandler<Aggregate, AggregateId, IExecutionResult, SetMagicNumberDistinctCommand>
+    {
+        public override Task<IExecutionResult> ExecuteCommandAsync(Aggregate aggregate, SetMagicNumberDistinctCommand command,
+            CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            IExecutionResult executionResult = aggregate.SetMagicNumber(command.MagicNumber);
+
+            return Task.FromResult(executionResult);
+        }
+    }
+}
